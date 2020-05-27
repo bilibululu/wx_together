3c182e54fee6ff2f8e5184c3d9548f170a2e7637
Lee <917546199@qq.com>
2020-05-27 20:24:38 +0800

    订阅消息

--- pages/show/show.js
+++ pages/show/show.js
@@ -8,7 +8,14 @@ Page({
     navFixed: false,
     currentData: 1,
     content1: '申请抱团',
-    content2: '已申请'
+    content2: '已申请',
+    //用于发布消息后加的
+    open_id:"openid",
+    model_id:"-qZ6a8B702CT0k_Tl3gPCpFkbqN91Xtu5mRjDbJN1fI",
+    name:"",
+    sId:"",
+    relation_way:"",
+    text:""
   },
 
   //获取动态展示
@@ -296,6 +303,9 @@ Page({
 
   apply: function (e) {
     var that = this;
+    var $msgid = e.currentTarget.dataset.id;
+    console.log($msgid)
+    var msgid=$msgid
     wx.showLoading({
       title: '申请抱团中',
     })
@@ -311,6 +321,123 @@ Page({
       },
       method: 'POST',
       success: function (res) {
+
+
+        that.setData({
+          name:res.data.data.name,
+          sId:res.data.data.sId,
+          relation_way:res.data.data.contact,
+          text:res.data.data.text
+        })
+        console.log('printname:',that.data.name)
+        console.log('printsId:',that.data.sId)
+        console.log('printcontact:',that.data.relation_way)
+        console.log('printtext',that.data.text)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////模板信息
+        //获取access_token
+          wx.request({
+            url:'https://together123.applinzi.com/together/index.php/Home/Accesstoken/getTokenCurl',
+            method: 'GET',
+            dataType: 'json',
+            success: function (res) {
+              app.globalData.access_token=res.data.access_token
+              console.log('access_token: ', res.data.access_token)
+              console.log('获取access_token成功')
+            },
+            fail: function (res) {
+              console.log('获取access_token失败')
+             },
+            complete: function (res) { 
+              console.log('获取access_token完成')
+            },
+          })
+        //获取发布消息对象的openid
+        var openid="";
+          wx.request({
+            url: 'https://together123.applinzi.com/together/index.php/Home/Readmsg/getPopenid',//给message_id传openid
+            method:'POST',
+            data:{
+                message_id:msgid
+            },
+            header: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            success:function(res){
+              console.log('pdata:',res.data)
+              console.log('popenid:',res.data.popenid)
+              openid=res.data.popenid
+              console.log('dopenid:',openid)
+            }
+          })
+
+          //向数据库申请姓名，学号，联系方式和动态内容
+          //模板id
+          var template = that.data.model_id;
+          //4个关键词
+          var keyword_name = that.data.name;
+          var keyword_sId = that.data.sId;
+          var keyword_rw = that.data.relation_way;
+          var keyword_text = that.data.text;
+          //printkey
+          console.log('name:',keyword_name)
+          console.log('sId:',keyword_sId)
+          console.log('relation_way:',keyword_rw)
+          console.log('text:',keyword_text)
+          //消息下发
+          var template_id ="-qZ6a8B702CT0k_Tl3gPCpFkbqN91Xtu5mRjDbJN1fI";
+          wx.requestSubscribeMessage({
+            tmplIds: ['-qZ6a8B702CT0k_Tl3gPCpFkbqN91Xtu5mRjDbJN1fI'],
+            success(res) {
+            //发送access_token请求
+              wx.request({
+                url: 'https://together123.applinzi.com/together/index.php/Home/Dingyue/sendMsg',
+                data:{
+                  access_token: app.globalData.access_token,
+                  //数据包
+                  data:{
+                  //openid
+                    "touser": openid,
+                  //模板id
+                    "template_id": template_id,
+                    "page": "index",
+                    "miniprogram_state": "developer",
+                    "lang": "zh_CN",
+                    "data": {
+                      "number1": {
+                        "value": msgid
+                      },
+                      "thing2": {
+                        "value": keyword_name
+                      },
+                      "number3": {
+                        "value": keyword_sId
+                      },
+                      "thing4": {
+                        "value": keyword_rw
+                      }
+                    }
+                  }
+                },
+                success: function(res) {
+                  console.log("订阅成功");
+                  console.log(res);
+                },
+                fail: function(res) {
+                  console.log("订阅失败");
+                },
+              })
+            }
+          })
+      
+
+   
+        
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
         console.log(res.data)
         wx.hideLoading()
         wx.showToast({
